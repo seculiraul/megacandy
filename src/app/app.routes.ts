0a96ba7e24d078c0bda1484fb4beab5c720291c9
Raul <raulsmarius@yahoo.com>
2024-02-26 14:09:44 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { CandyListComponent } from './components/candy-list/candy-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'list', component: CandyListComponent }];
